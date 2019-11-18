21d351326f091710abd1c52ad6b6175c81da9ad8
Mind Hunter <mackie1@Mind-Hunter-Macbook.local>
2019-11-18 17:58:48 +0200

    feat(): count laps

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let minutes = 0;
 let hours = 0;
 
 
+let displayMilliSeconds = 0;
 let displaySeconds = 0;
 let displayMinutes = 0;
 let displayHours = 0;
@@ -32,13 +33,15 @@ function stopWatch(){
         hours++;
     }
 
+    displayMilliSeconds = milliseconds.toString().length === 1 ? "0" + milliseconds.toString() : milliseconds.toString();
     displaySeconds = seconds.toString().length === 1 ? "0" + seconds.toString() : seconds.toString();
     displayMinutes = minutes.toString().length === 1 ? "0" + minutes.toString() : minutes.toString();
     displayHours = hours.toString().length === 1 ? "0" + hours.toString() : hours.toString();
 
-    document.getElementById("display").innerHTML = displayHours + ":" + displayMinutes + ":" + displaySeconds + ":" + milliseconds;
+    document.getElementById("display").innerHTML = displayHours + ":" + displayMinutes + ":" + displaySeconds + ":" + displayMilliSeconds;
 }
 
+var lapNumber = 1;
 function startStop(){
 
 if(status === "stopped"){
@@ -46,13 +49,24 @@ if(status === "stopped"){
     interval = window.setInterval(stopWatch, 10);
 
     document.getElementById("startStop").innerHTML = "Stop";
+    document.getElementById("startStop").style.backgroundColor="red";
+
 status = "started";
 }
 else{
+    var table = document.getElementById("laps");
+    var row = table.insertRow();
+    var cell2 = row.insertCell(0);
+    var cell1 = row.insertCell(0);
+    cell1.innerHTML = lapNumber;
+    cell2.innerHTML = displayHours + ":" + displayMinutes + ":" + displaySeconds + ":" + displayMilliSeconds;
+    lapNumber++;
 
-window.clearInterval(interval);
-document.getElementById("startStop").innerHTML = "Start";
-status = "stopped";
+
+    document.getElementById("startStop").style.backgroundColor="green";
+    window.clearInterval(interval);
+    document.getElementById("startStop").innerHTML = "Start";
+    status = "stopped";
 
 }
 
